ef97293fd59917ebf563aa5c9bd45dabc1a0d650
thuongpham09112000 <thuongpham09112000@gmail.com>
2024-11-16 14:57:18 +0700

    Update

--- chatbot/env.js
+++ chatbot/env.js
@@ -1,6 +1,6 @@
-export const process = {
-  env: {
-    OPENAI_API_KEY:
-      "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",
-  },
-};
+// export const process = {
+//   env: {
+//     OPENAI_API_KEY:
+//       "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",
+//   },
+// };
